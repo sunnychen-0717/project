2696ff8db301b7703050641241f13d5c7ff835d3
sunnychen-0717 <s1382229@live.hkmu.edu.hk>
2025-11-19 00:28:16 +0800

    Delete models directory

--- models/user.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const UserSchema = new mongoose.Schema({
-username: { type: String, required: true, minlength: 3, maxlength: 30, unique: true },
-password: { type: String, required: true, minlength: 3, maxlength: 100 },
-role: { type: String, enum: ['user','admin'], default: 'user' }
-});
-module.exports = mongoose.model('User', UserSchema);
